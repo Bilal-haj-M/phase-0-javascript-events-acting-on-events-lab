d434971c6c204d135ce572da73f810421f638306
Bilal-hajM <bblal1003@gmail.com>
2023-04-22 00:58:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+const dodger = document.getElementById("dodger")
+const speed = 1;
+
+function moveDodgerLeft() {
+  dodger.style.left = `${parseInt(dodger.style.left) -  speed}px`
+}
+
+function moveDodgerRight() {
+  dodger.style.left = `${parseInt(dodger.style.left) + speed}px`
+}
+
+function moveUp() {
+  dodger.style.top = `${parseInt(dodger.style.top) - speed}px`
+}
+
+function moveDown() {
+  dodger.style.top = `${parseInt(dodger.style.top) + speed}px`
+}
+
+document.addEventListener("keydown", function (event)  {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }else if (event.key === "ArrowRight") {
+    moveDodgerRight()
+  }else if (event.key === "ArrowUp") {
+    moveUp()
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
